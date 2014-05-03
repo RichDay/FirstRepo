ad2e9dff1cf938f4fd3f3d0cc88f0049d3bc5a89
RichDay <richdayrit@gmail.com>
2014-05-02 20:32:05 -0400

    working on last programming assignment

--- test.R
+++ test.R
@@ -1,4 +1,4 @@
-best <- function (statecall,outcomecall){
+rankhospital <- function (statecall,outcomecall,num="best"){
         ##read in the data
         path <- "C:\\Users\\Rich\\Documents\\Github\\FirstRepo\\outcome-of-care-measures.csv"
         data <- read.csv(path,stringsAsFactors=FALSE,na.strings="Not Available+")
@@ -11,9 +11,16 @@ best <- function (statecall,outcomecall){
         if (!any(data$State==statecall)) {
                 stop('invalid state')
         } 
+ 
+        
+        ##assign values for best
+        if (num=="best") {
+                num<-1
+        }
         
         ##filter by state
         statedata <- subset(data,State==statecall)
+        
         
         ##determine which column corresponds to outcome of interest     
         outcomeindex <- min(agrep(outcomecall,colnames(data),ignore.case=TRUE))
@@ -23,10 +30,22 @@ best <- function (statecall,outcomecall){
         answermatrix[,2] <- as.numeric(as.character(answermatrix[,2]))
         gooddata <- complete.cases(answermatrix)
         answermatrix <- answermatrix[gooddata,]
-        
-        ##find the hospital with the lowest score
-        answerindex <- which.min(answermatrix[,2])
-        besthospital <- answermatrix[answerindex,1]
 
-        print (besthospital)
+        ##sort the hospitals
+        colnames(answermatrix) <- c("hospital", "score")
+        r <- order(answermatrix$score,answermatrix$hospital)
+        sorted <- answermatrix[r,]
+
+        ##determine hospital selected in num
+        if (num=="worst") {
+                num<-nrow(sorted)
+        }
+        if (num>nrow(statedata)) {
+                answer=NA
+        } else {
+                answer<-sorted[num,1]    
+        }
+        
+        print(answer)
 }
+
